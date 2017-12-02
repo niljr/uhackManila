3dfc7dae0cab94f2b7aa31e6eec12545ced2f78e
ida <idaaureajimenez@gmail.com>
2017-12-03 04:55:19 +0800

    Intro

--- screens/IntroScreen.js
+++ screens/IntroScreen.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { AppRegistry, Alert, Image } from 'react-native';
 import Onboarding from 'react-native-onboarding-swiper';
+import Layout from '../constants/Layout';
 
 export default class Intro extends Component {
   static navigationOptions = {
@@ -15,9 +16,9 @@ export default class Intro extends Component {
     return (
       <Onboarding
         pages={[
-          { backgroundColor: '#fff', image: <Image source={require('../assets/images/robot-dev.png')}/>, title: 'Simple Messenger UI', subtitle: 'Implemented in React Native' },
-          { backgroundColor: "#fe6e58", image: <Image source={require('../assets/images/robot-dev.png')}/>, title: 'Welcome', subtitle: 'To Earth' },
-          { backgroundColor: "#999", image: <Image source={require('../assets/images/robot-dev.png')}/>, title: 'Also', subtitle: 'Mars is nice' },
+          { backgroundColor: '#fff', image: <Image source={require('../assets/images/baby1.jpg')}/>, title: 'Everyday playtime', subtitle: 'Fusce purus magna, maximus at pretium quis, convallis a tellus. Vestibulum facilisis aliquam venenatis.' },
+          { backgroundColor: "#fff", image: <Image source={require('../assets/images/toddler1.jpg')}/>, title: 'Unlimited choices', subtitle: 'Etiam bibendum a elit sed euismod. Aliquam efficitur mauris urna.' },
+          { backgroundColor: "#fff", image: <Image source={require('../assets/images/preSchooler1.jpg')}/>, title: 'So much fun!', subtitle: 'Nunc mattis risus sit amet magna mollis vulputate.' },
         ]}
         onSkip={this.goToLogin}
         onDone={this.goToLogin}
